bad632478fcfe91ba4405764e98af5e39f842fd2
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-02-16 10:59:49 -0800

    Making things look pretty

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,14 +3,19 @@ import React from 'react';
 import Constants from '../utils/constants.js';
 
 export default (props) => (
-  <nav className="navbar fixed-top navbar-dark bg-dark">
-    <a className="navbar-brand" href="/">{Constants.APP_NAME}</a>
+  <nav className="navbar fixed-top navbar-red justify-content-start">
+    <a className="navbar-brand" href="/"><img src="/navbar_logo.svg" alt="LiveSafe LA" /></a>
 
-    <form className="form-inline" onSubmit={props.searchAddress}>
-      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onChange = {props.updateAddress}/>
-      <button className="btn btn-outline-success my-2 my-sm-0" type="submit" onClick={props.searchAddress}>Search</button>
+    <form className="form-inline search-form" onSubmit={props.searchAddress}>
+      <div className="input-group">
+        <input className="form-control" type="search" placeholder="Search" aria-label="Search" onChange = {props.updateAddress}/>
+
+        <div className="input-group-append">
+          <button className="btn btn-search my-2 my-sm-0" type="submit" onClick={props.searchAddress}>Search</button>
+        </div>
+      </div>
     </form>
 
-    <a className="btn btn-primary" href="#">Sign Up</a>
+    <a className="btn btn-outline-light signup-btn" href="#">Sign Up</a>
   </nav>
 )
