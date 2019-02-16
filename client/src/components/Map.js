5b8ac42aa46cafc1d0b438e4a3eb1ff556107797
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-02-16 02:28:29 -0800

    Added ESRI map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
+import { Map } from '@esri/react-arcgis';
+
 export default (props) => (
-  <div className="map" />
+  <div className="map">
+    <Map
+      viewProperties={{
+        center: [-118.2851, 34.0224],
+        zoom: 15
+      }}
+    />
+  </div>
 )
