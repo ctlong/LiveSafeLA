56c35f624b850d4b379d0314afdd10d43726b30a
Carson Long <12767276+ctlong@users.noreply.github.com>
2019-02-16 03:21:07 -0800

    Got Socrata call working!!

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 const express = require('express');
+const https   = require('https');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/api/search', (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-  ];
+    var lat  = req.query.lat || 34.046;
+    var long = req.query.long || -118.2509;
 
-  res.json(customers);
+    https.get(`https://data.lacity.org/resource/7fvc-faax.json?$order=date_occ DESC&$where=within_circle(location_1,${lat},${long},5000)&crm_cd=110`, (rest) => {
+        var output = '';
+        console.log(rest.statusCode);
+
+        rest.on('data', function (chunk) {
+            output += chunk;
+        });
+
+        rest.on('end', function() {
+            var obj = JSON.parse(output);
+            res.json(obj);
+        });
+
+        //36.2827131
+        //-122.4072206
+    })
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
