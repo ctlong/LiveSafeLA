907593c178edcc8c9140ab9bd6cd534643f158b7
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-02-16 16:17:01 -0800

    Added autocomplete to dashboard navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,19 +1,51 @@
 import React from 'react';
 
-export default (props) => (
-  <nav className="navbar fixed-top navbar-red justify-content-start">
-    <a className="navbar-brand" href="/"><img src="/navbar_logo.svg" alt="LiveSafe LA" /></a>
-
-    <form className="form-inline search-form" onSubmit={props.searchAddress}>
-      <div className="input-group">
-        <input className="form-control" type="search" placeholder="Search" aria-label="Search" value={props.address} onChange = {props.updateAddress}/>
-
-        <div className="input-group-append">
-          <button className="btn btn-search my-2 my-sm-0" type="submit" onClick={props.searchAddress}>Search</button>
+export default class Navbar extends React.Component {
+  render() {
+    const suggestions = this.props.suggestions.map((suggestion, index) => {
+      return (
+        <div
+          className="suggestion"
+          key={suggestion.address + ' ' + index}
+          onClick={() => { this.props.selectSuggestion(suggestion) }}
+        >
+          {suggestion.address}
         </div>
-      </div>
-    </form>
+      )
+    })
 
-    <a className="btn btn-outline-light signup-btn" href="#">Sign Up</a>
-  </nav>
-)
+    const autocomplete = (this.props.showSuggestions) ? (
+      <div className="search-suggestions">
+        {suggestions}
+      </div>
+    ) : (null);
+
+    return (
+      <nav className="navbar fixed-top navbar-red justify-content-start">
+        <a className="navbar-brand" href="/"><img src="/navbar_logo.svg" alt="LiveSafe LA" /></a>
+
+        <form className="form-inline search-form" onSubmit={this.props.searchAddress}>
+          <div className="input-group">
+            <input
+              className="form-control"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+              value={this.props.address}
+              onChange = {this.props.updateAddress}
+              onFocus = {this.props.showSuggestionsAction}
+            />
+
+            <div className="input-group-append">
+              <button className="btn btn-search my-2 my-sm-0" type="submit" onClick={this.props.searchAddress}>Search</button>
+            </div>
+          </div>
+
+          {autocomplete}
+        </form>
+
+        <a className="btn btn-outline-light signup-btn" href="#">Sign Up</a>
+      </nav>
+    )
+  }
+}
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -7,14 +7,24 @@ import Navbar from '../components/Navbar';
 import Map from '../components/Map';
 import Sidebar from '../components/Sidebar';
 
+import { debounce } from "throttle-debounce";
+
+const FIND_ADDRESS_URL = 'http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?';
+
 class Dashboard extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       address: '',
+      coordinates: [],
+      suggestions: [],
+      showSuggestions: false,
       data: {}
     }
+
+    // Used for autocomplete to prevent making a call every time state.address is changed
+    this.autocompleteSearchDebounced = debounce(250, this.findAddressCandidates);
   }
 
   componentDidMount()  {
@@ -35,6 +45,17 @@ class Dashboard extends React.Component {
     });
   }
 
+  // Ping Esri find candidate API
+  findAddressCandidates = (address) => {
+    fetch(`${FIND_ADDRESS_URL}f=json&Address=${address}&city=Los Angeles&region=CA`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          suggestions: data.candidates.slice(0,5)
+        })
+      });
+  }
+
   _updateData = (data) => {
     this.setState({
       data
@@ -42,20 +63,78 @@ class Dashboard extends React.Component {
   }
 
   _updateAddress = (event) => {
+    const address = event.target.value;
+
+    const showSuggestions = (address === undefined || address === '') ? (false) : (true);
+
+    // Ping FIND_ADDRESS_URL and get results
     this.setState({
-      address: event.target.value
+      address,
+      showSuggestions
+    }, () => {
+      this.autocompleteSearchDebounced(this.state.address);
     });
   }
 
   _searchAddress = (event) => {
     event.preventDefault();
-    window.location.href = '/dashboard?address=' + this.state.address;
+
+    const address = this.state.address;
+
+    let x, y;
+
+    // If the user didn't use autocomplete, we need to manually ping the Esri API and use the first result
+    // Otherwise, we use whatever was selected and roll with it
+    if (this.state.coordinates.length !== 2) {
+      fetch(`${FIND_ADDRESS_URL}f=json&Address=${address}&city=Los Angeles&region=CA`)
+        .then(response => response.json())
+        .then(data => {
+          x = data.candidates[0].location.x;
+          y = data.candidates[1].location.y;
+          this.props.history.push(`/dashboard?address=${address}&x=${x}&y=${y}`);
+          document.location.reload();
+        });
+    } else {
+      x = this.state.coordinates[0];
+      y = this.state.coordinates[1];
+      this.props.history.push(`/dashboard?address=${address}&x=${x}&y=${y}`);
+      document.location.reload();
+    }
+  }
+
+  _hideSuggestions = () => {
+    this.setState({
+      showSuggestions: false
+    });
+  }
+
+  _showSuggestions = () => {
+    this.setState({
+      showSuggestions: true
+    });
+  }
+
+  _selectSuggestion = (suggestion) => {
+    this.setState({
+      address: suggestion.address,
+      coordinates: [suggestion.location.x, suggestion.location.y],
+      showSuggestions: false
+    })
   }
 
   render() {
     return (
       <div>
-        <Navbar address={this.state.address} updateAddress={this._updateAddress} searchAddress={this._searchAddress} />
+        <Navbar
+          address={this.state.address}
+          updateAddress={this._updateAddress}
+          searchAddress={this._searchAddress}
+          hideSuggestionsAction={this._hideSuggestions}
+          showSuggestionsAction={this._showSuggestions}
+          showSuggestions={this.state.showSuggestions}
+          selectSuggestion={this._selectSuggestion}
+          suggestions={this.state.suggestions}
+        />
 
         <div className="container-fluid main">
           <div className="row">
