6cdd4941b2a6fb16b7b02b0ca166cc97c1b175f2
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-02-16 22:28:22 -0800

    Kill me now

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const app_token = (process.env.APP_TOKEN ? `$$app_token=${process.env.APP_TOKEN}&` : '');
-const reqString = `https://data.lacity.org/resource/7fvc-faax.json?${app_token}$limit=1000&$order=date_occ DESC&$select=date_occ, time_occ, crm_cd, vict_age, vict_sex, weapon_used_cd, location_1&`;
+const reqString = `https://data.lacity.org/resource/7fvc-faax.json?${app_token}$limit=1000&$order=date_occ DESC&$select=date_occ, time_occ, crm_cd, vict_age, vict_sex, weapon_used_cd, location_1, location&`;
 const radius    = 800;
 const messages  = [
     'This area is safe. Wander freely. Call 911 if there is an emergency.',
